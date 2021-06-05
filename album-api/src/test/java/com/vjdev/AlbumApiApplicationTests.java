ef53b1f055c34c628928771fd237b238af8b7d02
John <vjohnvictor@hotmail.com>
2021-06-05 13:44:41 +0530

    album api initial commit

---
+++ album-api/src/test/java/com/vjdev/AlbumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vjdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
